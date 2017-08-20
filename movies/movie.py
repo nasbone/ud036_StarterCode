beecc97c347c9a0720f23ee0a1dc76a415891dd5
Abdulnasir <nasbone@users.noreply.github.com>
2017-08-19 22:18:10 -0400

    added comments

--- movies/movie.py
+++ movies/movie.py
@@ -1,15 +1,22 @@
 import webbrowser
 
-
 class Movie():
-    """
     
     """
-
+    Store abstract movie attributes
+    """
     def __init__(self, movie):
+        
+        """
+        instantiates a movie object with the properties below
+        """
         self.poster = movie['poster']
         self.title = movie['title']
         self.trailer = movie['trailer']
 
     def show_trailer(self):
+        
+        """
+        a method to play a trailer of a movie via a web browser
+        """
         webbrowser.open(self.trailer)
