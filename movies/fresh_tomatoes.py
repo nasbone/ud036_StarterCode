7427f8f8e16b951e302a2c28a6a8963bd6efba5e
Abdulnasir <nasbone@users.noreply.github.com>
2017-08-19 22:19:44 -0400

    refactored create_movies_tiles_content function

--- movies/fresh_tomatoes.py
+++ movies/fresh_tomatoes.py
@@ -133,15 +133,13 @@ def create_movie_tiles_content(movies):
     # The HTML content for this section of the page
     content = ''
     for movie in movies:
-
-
+        
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster,
             trailer_youtube_id=movie.trailer
         )
-    print content
     return content
 
 
